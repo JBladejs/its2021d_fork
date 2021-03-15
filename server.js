4b1f192ca2d1aaa0a0c7b61ef0dab1ab8a873fd4
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2021-03-15 15:44:26 +0100

    Serwer statycznego kontentu

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('Server - wersja master')
+// moduły wbudowane
+const http = require('http')
+
+// moduły dodatkowe
+const static = require('node-static')
+
+// inicjalizacja globalnych obiektów
+const httpServer = http.createServer()
+const fileServer = new static.Server('./public')
+
+// zdefiniowanie reakcji na request http
+httpServer.on('request', function(req, res) {
+    console.log(req.method, req.url)
+    fileServer.serve(req, res)
+})
+
+httpServer.listen(8888)
