dda49227b40c62727777474854655cc49e251e98
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2021-06-14 16:19:28 +0200

    Okienko opisu projektu

--- public/js/projectDescription.js
+++ public/js/projectDescription.js
@@ -2,8 +2,14 @@ app.controller('ProjectDescription', [ '$http', '$uibModalInstance', 'options', 
     let ctrl = this
     console.log('Kontroler ProjectDescription wystartował')
     ctrl.options = options
+    ctrl.persons = []
 
     ctrl.ok = function() {
         $uibModalInstance.close()
     }
+
+    $http.get('/person').then(function(res) {
+        // poniżej coś wstydliwego - filtrowanie we frontendzie
+        ctrl.persons = res.data.records.filter(function(el) { return el.projects.map(function(el) { return el._id }).includes(ctrl.options.project._id) })
+    }, function(err) {})
 }])
